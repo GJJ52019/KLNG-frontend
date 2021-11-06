ce6dcf28a4abc46929d77b14e2261be890de912f
kimmiec <89497794+kimmiec@users.noreply.github.com>
2021-11-05 20:35:04 -0400

    start to add functions on main.js

--- klng-frontend/src/components/Main.js
+++ klng-frontend/src/components/Main.js
@@ -13,6 +13,18 @@ function Main(props){
 
     const URL = 'https://localhost:3000/';
 
+    const getHome = async() => {
+        const response = await fetch (`${URL}`);
+        const data = await response.json();
+        setHome(data);
+    }
+
+    // const createHome = async (post) =>{
+    //     await fetch (`${URL}`)
+    // }
+
+    // ^^^how do we create something if we dont have a create route backend?? 
+
     return (
         <div className='main'>
             <Routes>
