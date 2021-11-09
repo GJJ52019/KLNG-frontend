510a0684e972a4eb059f47ec0017acfab7431e43
kimmiec <89497794+kimmiec@users.noreply.github.com>
2021-11-08 22:00:09 -0500

    start the info access on show project

--- klng-frontend/src/pages/ShowProject.js
+++ klng-frontend/src/pages/ShowProject.js
@@ -1,21 +1,28 @@
 import Nav from "../components/Nav";
+import { useParams } from 'react-router'
 
 function ShowProject(props){
+    // need to import useParams to use params (params.id) now
+    const params = useParams();
+    const id = props.match.params.id;
+    const projects = props.projects
+    const project = projects.find(p => p.id === id)
+
     return (
         <div className="ProjDiv">
             <Nav />
             <h1>SHOW PROJECT PAGE</h1>
             <div className="titleDiv">
                 <h3>avatar image here</h3>
-                <h1>name here</h1>
+                {/* <h1> {project.project_name} </h1> */}
             </div>
 
             <div className="projDescripDiv">
-                <h4>project desc here</h4>
+                {/* <h4>p{project.desc}</h4> */}
             </div>
 
             <div className="screenShotDiv">
-                <h4>screenshot of specific project here</h4>
+                {/* <img src={project.image_url} alt={project.project_name} /> */}
             </div>
 
 
